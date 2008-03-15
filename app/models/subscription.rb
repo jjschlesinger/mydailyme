a821d60341d19f76a0f3d674e1159cc5d76c451d
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-03-15 10:04:14 +0000

    fix last y pos
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@46 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,11 @@ class Subscription < ActiveRecord::Base
   belongs_to :me
   
   def self.find_last_y_pos(user_id)
-    Subscription.find(:first, :conditions => "user_id = #{user_id} and pos_x = 0", :order => "pos_y DESC").pos_y
+    s = Subscription.find(:first, :conditions => "user_id = #{user_id} and pos_x = 0", :order => "pos_y DESC")
+    if s.nil?
+      0
+    else
+      s.pos_y
+    end
   end
 end
