3817e3b1855b2ba447f02a2ae9ce7954418443f6
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-03-21 00:49:34 +0000

    stupid ie fix
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@74 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- public/javascripts/portal.js
+++ public/javascripts/portal.js
@@ -59,14 +59,4 @@ function show_hide(id)
     }
 }
 
-function resize(which, max) {
-  var elem = document.getElementById(which);
-  if (elem == undefined || elem == null) return false;
-  if (max == undefined) max = 100;
-  if (elem.width > elem.height) {
-    if (elem.width > max) elem.width = max;
-  } else {
-    if (elem.height > max) elem.height = max;
-  }
-
-}
+var max = 150;
