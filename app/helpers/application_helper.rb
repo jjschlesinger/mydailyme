5a214c6fbd256163fefc4b38251331761ad1e06d
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-03-28 09:01:04 +0000

    added rss link and helper for netvibes link
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@89 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
-# Methods added to this helper will be available to all templates in the application.
+require 'cgi'
+
 module ApplicationHelper
+    def netvibes_feed_link(login)
+        login = CGI.escape(login)
+        return '<a href="http://www.netvibes.com/subscribe.php?feed=http%3A//' + CGI.escape(login) + '@me.projecttruck.net/subscriptions.rss" target="_blank"><img src="http://www.netvibes.com/img/add2netvibes.gif" border="none" width="91" height="17" alt="Add to Netvibes"  /></a>'
+    end
 end
