b2e14bfd4ae41e89da77401a6cc74dc2f5278525
adam <adam@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-08-29 20:05:49 +0000

    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@96 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     if request.xhr? and request.post?
       @me = Me.find params[:id]
       @me.comments << Comment.create(:user_id => session['user_id'], :message => params[:comment_text])
-      render :layout => false
+      render :partial => "comments"
     else
       redirect_to '/'
     end
