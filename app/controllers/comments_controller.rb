26dcdb81235e5cefc40aa312bbf92ab1a2ed1694
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-08-29 20:49:34 +0000

    fixed ajax
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@97 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < ApplicationController
     if request.xhr? and request.post?
       @me = Me.find params[:id]
       @me.comments << Comment.create(:user_id => session['user_id'], :message => params[:comment_text])
-      render :partial => "comments"
+      #@result = @me.comments.to_json
+      #render :layout => false
+      render :partial => "comment_list", :locals => { :comments => @me.comments }, :layout => false
     else
       redirect_to '/'
     end
