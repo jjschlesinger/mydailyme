18e10f4530f305b5c0bfc6704eb4c3aa3920936d
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-08-30 10:34:35 +0000

    fixed validation for new comment
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@100 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,25 @@ class CommentsController < ApplicationController
 
   def create
     if request.xhr? and request.post?
-      if params[:comment_text].length > 0
+      #if params[:comment_text].length > 0
       collapse = false
       if request.post?
       	collapse = true
       end 
       @subscription = Subscription.find params[:id]
-      @subscription.me.comments << Comment.create(:user_id => session['user_id'], :message => params[:comment_text])
+     @subscription.me.comments << Comment.create!(:user_id => session['user_id'], :message => params[:comment_text])
 
-      dontCollapse = false
+	  dontCollapse = false
 
-      #@result = @me.comments.to_json
-      #render :layout => false
-      render :partial => "subscriptions/comments", :locals => { :subscription => @subscription }, :layout => false
-      end
-    else
-      redirect_to '/'
+	  #@result = @me.comments.to_json
+	  #render :layout => false
+	  render :partial => "subscriptions/comments", :locals => { :subscription => @subscription }, :layout => false
+	  
+      
     end
+    
+    rescue ActiveRecord::RecordInvalid => e
+      	render :partial => "subscriptions/comments", :locals => { :subscription => @subscription, :error_message => e.record.errors.full_messages.join(", ").sub("Message ", "") }, :layout => false, :status => 444
   end
   
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :me
   belongs_to :user
+  validates_presence_of :message, :message=>"Please enter a comment"
+  validates_length_of :message, :maximum => 1, :too_short => "Please enter a comment", :maximum => 1000, :too_long=>"Please limit your comment to %d characters"
 end
