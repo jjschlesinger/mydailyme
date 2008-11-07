ad34427d265a8bcec0c6fb7310a43aa5224251a1
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-11-07 22:09:08 +0000

    fixed thumbnail bug
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@121 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/lib/me_image.rb
+++ app/lib/me_image.rb
@@ -6,6 +6,7 @@ require 'RMagick'
 class MeImage
 
 	def self.grab_image(me_id, img_url)
+		thumb_geometry = "150x150"
 		me_images_dir = "me_images"
 		me_images_root = "#{RAILS_ROOT}/public/#{me_images_dir}"
 		Dir.mkdir(me_images_root) unless File.exist?(me_images_root)
@@ -15,27 +16,13 @@ class MeImage
 		img_ext = img_name[img_name.rindex(".")..-1]
 		new_img = "#{me_images_root}/me#{me_id}#{img_ext}"
 		new_img_relative = 	"/#{me_images_dir}/me#{me_id}#{img_ext}"	
-		open(new_img, "wb").write(open(img_url).read)
-
+		open(new_img,"wb") {|f|
+			f.write open(img_url).read } 
 		img = Magick::Image.read(new_img).first
-
-		max = 150.0
-		width = img.columns
-		height = img.rows
-		zoom = 0.0
-
-		if width / max > height / max
-			zoom = max / width
-		else
-			zoom = max / height
-		end
-
-		new_width = width * zoom
-		new_height = height * zoom
-
-		img.thumbnail!(new_width, new_height)
-		img.write(new_img)
-
+		img.change_geometry!(thumb_geometry) { |cols, rows, img2|
+		 img2.thumbnail!(cols, rows)
+		 img2.write(new_img)
+		 }		
 		new_img_relative
 	end
 end
