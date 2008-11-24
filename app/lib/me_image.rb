eabdec778ab9b0bb05944b7ba78e44526e8d49f5
josh <josh@5169b2cb-b947-0410-aaf8-ca3d8c07b963>
2008-11-24 22:25:20 +0000

    fixed me image caching problem
    git-svn-id: https://jschlesinger.net/svn/projectme/trunk@137 5169b2cb-b947-0410-aaf8-ca3d8c07b963

--- app/lib/me_image.rb
+++ app/lib/me_image.rb
@@ -9,6 +9,7 @@ class MeImage
 		width = 150
 		height = 150
 		thumb_geometry = "#{width}x#{height}"
+		token = Time.now.to_f.to_s
 		me_images_dir = "me_images"
 		me_images_root = "#{RAILS_ROOT}/public/#{me_images_dir}"
 		Dir.mkdir(me_images_root) unless File.exist?(me_images_root)
@@ -16,8 +17,13 @@ class MeImage
 		path = uri.path.split("/")
 		img_name = path[path.length - 1]
 		img_ext = img_name[img_name.rindex(".")..-1]
-		new_img = "#{me_images_root}/me#{me_id}#{img_ext}"
-		new_img_relative = 	"/#{me_images_dir}/me#{me_id}#{img_ext}"	
+		img_name = "me#{me_id}_#{token}#{img_ext}"
+		new_img = "#{me_images_root}/#{img_name}"
+		new_img_relative = 	"/#{me_images_dir}/#{img_name}"
+		Dir.chdir(me_images_root)	
+		Dir["me#{me_id}_*"].each do |f|
+			File.delete(f)
+		end
 		open(new_img,"wb") {|f|
 			f.write open(img_url).read 
 		}
