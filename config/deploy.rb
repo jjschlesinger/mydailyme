08cd356a2117f5e0abd87694d849521b74dccd03
Josh Schlesinger <josh@jschlesinger.net>
2009-03-14 16:19:49 -0500

    added cap files and removed migration

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "beta.mydailyme"
+set :repository,  "git@github.com:jjschlesinger/mydailyme.git"
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :copy
+set :scm, :git
+set :host, "slice1.mydailyme.net"
+set :port, 22000
+ssh_options[:paranoid] = false
+set :use_sudo, false
+#set :user, "www-data"
+
+
+role :app, host
+role :web, host
+role :db,  host, :primary => true
+
+task :after_symlink, :roles => :app do
+	run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+	
+end
--- db/migrate/20081107002540_add_me_image_thumbnail.rb
+++ db/migrate/20081107002540_add_me_image_thumbnail.rb
@@ -1,9 +1,9 @@
 class AddMeImageThumbnail < ActiveRecord::Migration
   def self.up
-	add_column :mes, :image_thumbnail, :string
+		#add_column :mes, :image_thumbnail, :string
   end
 
   def self.down
-  	remove_column :mes, :image_thumbnail
+  	#remove_column :mes, :image_thumbnail
   end
 end
