001f021caf405a4fd100a929dabb686f0b61e1f2
Josh Schlesinger <josh@jschlesinger.net>
2009-04-14 08:14:38 -0500

    changed session expirey to 24.hours fixes #60

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,7 +7,7 @@
 ActionController::Base.session = {
   :key         => '_mydailyme_session',
   :secret      => 'a0d0ab5539d87c66c4b3ee1595be40c52e4e5d022cda5acb43d5ca0d3704ce9447e21a810a0a014481dd0138986f4ff1c85b07be381173f5edc8006370f989ae',
-  :expire_after => 120.minutes
+  :expire_after => 24.hours
 }
 
 # Use the database for sessions instead of the cookie-based default,
